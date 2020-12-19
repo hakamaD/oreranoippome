d095d2f191e3648b63c7bcc33894da2f9d61df8c
yuzu0730 <takumada0730@gmail.com>
2020-12-19 21:56:20 +0900

    nodejs2

---
+++ nodejs2_routing.js
@@ -0,0 +1,16 @@
+// パッケージを読み込む
+let http = require('http');
+
+// サーバ機能の初期化
+let server = http.createServer();
+
+// リクエストがきたときに呼び出される
+server.on('request', function(req, res)
+{
+    // 本文（Body部）に、呼び出されたURL（http://localhost:1337以降の部分）と一緒に文字を表示する
+    res.write('Hello, ' + req.url);
+    res.end();
+});
+
+// 指定したIPアドレス、ポート番号でサーバを立てる
+server.listen(8000, 'ip-10-0-0-33.ap-northeast-1.compute.internal.0.1');
